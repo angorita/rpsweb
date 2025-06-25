1bde1b392f33974ec6bad5eb8a0f1f7e2ab9597e
oscar <oaangarita@yahoo.com.ar>
2025-06-24 23:53:45 -0300

    Siguiente commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,21 +11,42 @@ const (
 	templateBase = templateDir + "base.html"
 )
 
+type Player struct {
+	Name string
+}
+
+var player Player
+
 // para enviar datos a una plantilla en go necesitamos una estructura,mapas u otros
 // manejador
 func Index(w http.ResponseWriter, r *http.Request) {
+	RestartValue()
 	RenderTemplate(w, "index.html", nil)
 }
 func NewGame(w http.ResponseWriter, r *http.Request) {
+	RestartValue()
 	RenderTemplate(w, "new-game.html", nil)
 }
 func Game(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "game.html", nil)
+	if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error Parsing form", http.StatusBadRequest)
+			return
+		}
+		//capturo name de new-game.html
+		player.Name = r.Form.Get("name")
+	}
+	if player.Name == "" {
+		http.Redirect(w, r, "/new", http.StatusFound)
+	}
+	RenderTemplate(w, "new-game.html", player)
 }
-func Play(w http.ResponseWriter, r *http.Request){
-	RenderTemplate(w,"jugar.html",nil)
+func Play(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "jugar.html", nil)
 }
 func About(w http.ResponseWriter, r *http.Request) {
+	RestartValue()
 	RenderTemplate(w, "about.html", nil)
 }
 func RenderTemplate(w http.ResponseWriter, page string, data any) {
@@ -36,4 +57,9 @@ func RenderTemplate(w http.ResponseWriter, page string, data any) {
 		log.Println(err)
 		return
 	}
+}
+//reiniciar valores
+func RestartValue(){
+	player.Name=""
+
 }
